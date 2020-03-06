ef2666979bd501751b23711dc8f113e407c971e1
Henry Hu <henry.hu.sh@gmail.com>
2020-03-05 23:05:09 -0500

    migrate patches

--- ui/style/style_core_font.cpp
+++ ui/style/style_core_font.cpp
@@ -22,7 +22,7 @@ void style_InitFontsResource() {
 	Q_INIT_RESOURCE(win);
 #elif defined Q_OS_MAC // Q_OS_WIN
 	Q_INIT_RESOURCE(mac);
-#elif defined Q_OS_LINUX && !defined DESKTOP_APP_USE_PACKAGED // Q_OS_WIN || Q_OS_MAC
+#elif (defined Q_OS_LINUX || defined Q_OS_FREEBSD) && !defined DESKTOP_APP_USE_PACKAGED // Q_OS_WIN || Q_OS_MAC
 	Q_INIT_RESOURCE(linux);
 #endif // Q_OS_WIN || Q_OS_MAC || (Q_OS_LINUX && !DESKTOP_APP_USE_PACKAGED)
 }
