6abd3c60f8c684b6ba4f2086077b7cac59ca595c
Henry Hu <henry.hu.sh@gmail.com>
2020-03-05 21:58:03 -0500

    freebsd patch

--- ui/platform/ui_platform_utility.h
+++ ui/platform/ui_platform_utility.h
@@ -37,7 +37,7 @@ void DrainMainQueue(); // Needed only if UseMainQueueGeneric() is false.
 
 #ifdef Q_OS_MAC
 #include "ui/platform/mac/ui_utility_mac.h"
-#elif defined Q_OS_LINUX // Q_OS_MAC
+#elif defined Q_OS_LINUX || defined Q_OS_FREEBSD // Q_OS_MAC
 #include "ui/platform/linux/ui_utility_linux.h"
 #elif defined Q_OS_WINRT || defined Q_OS_WIN // Q_OS_MAC || Q_OS_LINUX
 #include "ui/platform/win/ui_utility_win.h"
